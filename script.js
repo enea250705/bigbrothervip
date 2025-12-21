67232de96aa7c0af9bf2b5336d7fabfc396c148e
enea250705 <enea250705@users.noreply.github.com>
2025-12-21 17:16:16 +0100

    Update OK.ru embed URLs for both channels and improve desktop popunder support

--- script.js
+++ script.js
@@ -109,8 +109,8 @@ const mainVideoPlayer = {
 // with full controls (play, pause, volume, fullscreen, etc.)
 // ============================================
 const streamUrls = {
-    1: 'https://ok.ru/video/10632798674630', // Kanali 1 - OK.ru live stream
-    2: '//ok.ru/videoembed/10635883912902?nochat=1&autoplay=1'  // Kanali 2 - OK.ru live stream (iframe only, no video element)
+    1: '//ok.ru/videoembed/10641169915590?nochat=1&autoplay=1', // Kanali 1 - OK.ru live stream
+    2: '//ok.ru/videoembed/10641176665798?nochat=1&autoplay=1'  // Kanali 2 - OK.ru live stream
 };
 
 // Stream types configuration (auto-detected, but can be manually set)
